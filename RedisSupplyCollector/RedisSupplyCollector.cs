89db6cb0d758bf9866b21ca2e9c153607e662fd9
Ivan-2NE1 <ivang.2ne1@yahoo.com>
2019-08-20 18:58:50 +0300

    structure

---
+++ RedisSupplyCollector/RedisSupplyCollector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using S2.BlackSwan.SupplyCollector;
+using S2.BlackSwan.SupplyCollector.Models;
+
+namespace RedisSupplyCollector
+{
+    public class RedisSupplyCollector : SupplyCollectorBase
+    {
+        public override List<string> CollectSample(DataEntity dataEntity, int sampleSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<string> DataStoreTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<DataCollectionMetrics> GetDataCollectionMetrics(DataContainer container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override (List<DataCollection>, List<DataEntity>) GetSchema(DataContainer container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool TestConnection(DataContainer container)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RedisSupplyCollectorTests/RedisSupplyCollectorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RedisSupplyCollectorTests
+{
+    public class RedisSupplyCollectorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
