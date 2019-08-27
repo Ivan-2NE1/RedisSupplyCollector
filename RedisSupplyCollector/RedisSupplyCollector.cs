7516ad322d446dc38fe8138878f5451bb24a83ca
marician-2ne1 <ivang.2ne1@yahoo.com>
2019-08-27 12:44:20 -0400

    unit test

--- RedisSupplyCollector/RedisSupplyCollector.cs
+++ RedisSupplyCollector/RedisSupplyCollector.cs
@@ -1,35 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using S2.BlackSwan.SupplyCollector;
 using S2.BlackSwan.SupplyCollector.Models;
+using StackExchange.Redis;
 
 namespace RedisSupplyCollector
 {
     public class RedisSupplyCollector : SupplyCollectorBase
     {
+        public string BuildConnectionString(Dictionary<string, string> connectionStringValues)
+        {
+            return connectionStringValues["host"] + ":" + connectionStringValues["port"];
+        }
         public override List<string> CollectSample(DataEntity dataEntity, int sampleSize)
         {
-            throw new NotImplementedException();
+            DataContainer container = dataEntity.Container;
+            var redis = ConnectionMultiplexer.Connect(container.ConnectionString);
+            IDatabase db = redis.GetDatabase();
+            IServer server = redis.GetServer(container.ConnectionString);
+
+            var samples = new List<string>();
+
+            foreach(var key in server.Keys())
+            {
+                RedisValue value = db.StringGet(key);
+                samples.Add(value.ToString());
+            }
+
+            return samples;
         }
 
         public override List<string> DataStoreTypes()
         {
-            throw new NotImplementedException();
+            return new List<string>() { "Redis" };
         }
 
         public override List<DataCollectionMetrics> GetDataCollectionMetrics(DataContainer container)
         {
-            throw new NotImplementedException();
+            var dataCollectionMetrics = new List<DataCollectionMetrics>();
+
+            var redis = ConnectionMultiplexer.Connect(container.ConnectionString);
+            IDatabase db = redis.GetDatabase();
+            IServer server = redis.GetServer(container.ConnectionString);
+
+            foreach(var key in server.Keys())
+            {
+                RedisValue value = db.StringGet(key);
+
+                var metrics = new DataCollectionMetrics();
+                metrics.Name = key;
+                metrics.RowCount = 1;
+                metrics.TotalSpaceKB = value.Length() / 1024;
+                metrics.UsedSpaceKB = metrics.TotalSpaceKB;
+
+                dataCollectionMetrics.Add(metrics);
+            }
+
+            return dataCollectionMetrics;
         }
 
         public override (List<DataCollection>, List<DataEntity>) GetSchema(DataContainer container)
         {
-            throw new NotImplementedException();
+            var collections = new List<DataCollection>();
+            var entities = new List<DataEntity>();
+
+            var redis = ConnectionMultiplexer.Connect(container.ConnectionString);
+            IDatabase db = redis.GetDatabase();
+            var endpoints = redis.GetEndPoints();
+            IServer server = redis.GetServer(endpoints.First());
+
+            foreach(var key in server.Keys())
+            {
+                var collectionEntities = new List<DataEntity>();
+
+                DataCollection dataCollection = new DataCollection(container, key);
+                dataCollection.Container = container;
+                dataCollection.Name = key;
+
+                collections.Add(dataCollection);
+
+                RedisValue value = db.StringGet(key);
+                if(value.HasValue)
+                {
+                    var redisdataType = DataType.String.ToString();
+                    entities.Add(new DataEntity(value.ToString(), DataType.String, redisdataType, container, dataCollection));
+                }
+
+            }
+
+            return (collections, entities);
         }
 
         public override bool TestConnection(DataContainer container)
         {
-            throw new NotImplementedException();
+            var connection = ConnectionMultiplexer.Connect(container.ConnectionString);
+            return connection.IsConnected;
         }
     }
 }
--- RedisSupplyCollectorTests/RedisSupplyCollectorTests.cs
+++ RedisSupplyCollectorTests/RedisSupplyCollectorTests.cs
@@ -1,14 +1,53 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using S2.BlackSwan.SupplyCollector.Models;
 
 namespace RedisSupplyCollectorTests
 {
     public class RedisSupplyCollectorTests
     {
-        [Fact]
-        public void Test1()
+        public readonly RedisSupplyCollector.RedisSupplyCollector _instance;
+        public readonly DataContainer _container;
+        
+        public RedisSupplyCollectorTests()
         {
+            _instance = new RedisSupplyCollector.RedisSupplyCollector();
+            var connectionStringValues = new Dictionary<string, string>();
+            connectionStringValues["host"] = "localhost";
+            connectionStringValues["port"] = "6379";
 
+            _container = new DataContainer()
+            {
+                ConnectionString = _instance.BuildConnectionString(connectionStringValues)
+            };
         }
+
+        [Fact]
+        public void DataStoreTypesTest()
+        {
+            var result = _instance.DataStoreTypes();
+            Assert.Contains("Redis", result);
+        }
+
+        [Fact]
+        public void TestConnectionTest()
+        {
+            var result = _instance.TestConnection(_container);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetSchemaTest()
+        {
+            var (tables, elements) = _instance.GetSchema(_container);
+            Assert.Equal(3, tables.Count);
+            Assert.Equal(120, elements.Count);
+            foreach(DataEntity element in elements)
+            {
+                Assert.NotEqual(string.Empty, element.DbDataType);
+            }
+        }
+
     }
 }
